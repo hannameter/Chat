3eda5f4a16e6333941062051edda24e65ca7e5e5
Kim Hanna <hannameter@gmail.com>
2023-12-02 14:15:12 +0900

    using SingalR, chat server basic configuration

---
+++ Chat/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task NewMessage(long username, string message) =>
+        await Clients.All.SendAsync("messageReceived", username, message);
+    }
+}
--- Chat/Program.cs
+++ Chat/Program.cs
@@ -1,5 +1,16 @@
+using Chat.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:1000");
+builder.Services.AddSignalR();
+
 var app = builder.Build();
-app.UseWebSockets(); // Adds the WebSocketMiddleware.
+
+app.UseDefaultFiles(); // Enables default file mapping on the current path.
+app.UseStaticFiles(); // Enables static file serving for the current request path.
+// The preceding code allows the server to locate and serve the index.html file. 
+app.MapHub<ChatHub>("/hub");
+
+
 await app.RunAsync();
+
